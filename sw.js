047982f9de098e3b1cc869d079f4d0aabf2921bd
SpykeTRG <spyke.trg@gmail.com>
2026-07-18 07:47:39 +0500

    Update service worker cache version and cleanup logic
    Updated CACHE_NAME to version 3 and added automatic cache cleanup for old versions.

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'gy6-telemetry-v2';
+// ПРИ ОБНОВЛЕНИИ ПРОСТО МЕНЯЙТЕ v1 НА v2, v3 И Т.Д.
+const CACHE_NAME = 'gy6-telemetry-v3'; 
+
 const ASSETS = [
   'index.html',
   'style.css',
@@ -10,6 +12,23 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// АВТОМАТИЧЕСКОЕ УДАЛЕНИЕ СТАРЫХ ВЕРСИЙ КЭША
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          // Если имя старого кэша в памяти телефона не совпадает с текущим CACHE_NAME — удаляем его
+          if (cache !== CACHE_NAME) {
+            console.log('Удаление старого кэша:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
